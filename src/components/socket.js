e7eb7362596b8fcf0d959338482c2c11e748dd63
Abinash Biswal <abinashbiswal248@gmail.com>
2021-09-10 17:32:58 +0530

    ENH-03: Added changes to create socket-client using SD pattern

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,4 +1,4 @@
 import clientSocket from "socket.io-client";
 
-export const API_URL = "http://localhost:3001";
-export const socket = clientSocket(`${API_URL}`);
+export const SOCKET_URL = "http://localhost:3001";
+export const socket = clientSocket(SOCKET_URL);
